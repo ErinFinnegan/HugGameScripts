bb5eb64b490d4309db2bb95d459445c973812bae
ErinFinnegan <eef206@nyu.edu>
2015-05-06 22:48:33 -0400

    added semi-working game over screen

---
+++ ScreenController.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+
+var onoff : boolean; 
+var screenObject : GameObject;
+
+//function Start () {
+
+//}
+
+
+function Update () { 
+
+
+if (GetComponent(PlayerMovement).currentRep < 0){
+//	onoff == true;
+  	screenObject.SetActive(true); 
+    
+    } else {
+    
+ //   onoff == false;
+    screenObject.SetActive(false); 
+       
+    }
+    
+}
+
